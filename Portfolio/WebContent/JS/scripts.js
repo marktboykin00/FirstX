7f38876c2c49d2af7d93f112c38debc41626e2a4
markt <markt@10.135.44.2>
2019-10-21 21:13:20 -0500

    New New

--- Portfolio/WebContent/JS/scripts.js
+++ Portfolio/WebContent/JS/scripts.js
@@ -1,3 +1,35 @@
-/**
- * 
- */
+window.onload = function() {
+
+	document.getElementsByClassName("mobilemenutrigger")[0].addEventListener("click", mobileMenuFunc);
+
+	document.getElementsByClassName("exit")[0].addEventListener("click", closeFunc);
+
+	document.getElementById("mobilemenu").addEventListener("click", closeFunc);
+
+}
+
+
+
+function mobileMenuFunc() {
+
+	var mobile = document.getElementById("mobilemenucontainer");
+
+	mobile.style.display = "flex";
+
+	mobile.className = "fadeinClass";
+
+	document.body.style.overflow = "hidden";
+
+}
+
+
+
+function closeFunc() {
+
+	var mobile = document.getElementById("mobilemenucontainer");
+
+	mobile.style.display = "none";
+
+	document.body.style.overflow = "visible";
+
+}
